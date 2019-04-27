a13d904d87950ff137052ee04680239029af7b5e
Erin Ponsonby <erinponsonby@gmail.com>
2019-04-27 12:56:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,10 +5,10 @@ class Shoe
   def initialize(brand)
     @brand = brand
       if BRANDS.include?(brand)
-        
+      else
+      BRANDS << brand
     end
   end
-  
   BRANDS = []
   
   def cobble
