57c274690f7fa29331acccf72512e41ffe3453ad
Erin Ponsonby <erinponsonby@gmail.com>
2019-04-27 12:50:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,10 +4,8 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS.include?(brand)
-      break
-    else
-      BRANDS << brand
+      if BRANDS.!include?(brand)
+        BRANDS << brand 
     end
   end
   BRANDS = []
